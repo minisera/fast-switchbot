9967c1d2f7bc81bc6dfc8dd2f8b8b64287f2a068
minisera <koki.ohtaka0821@gmail.com>
2023-07-15 14:31:59 +0900

    refactor: Header取得のメソッドを切り分ける

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,96 +2,102 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.request.headers
 import io.ktor.client.request.request
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
+import io.ktor.http.Headers
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import io.ktor.http.contentType
+import io.ktor.http.headers
 import io.ktor.serialization.kotlinx.json.json
+import java.lang.RuntimeException
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.Serializable
 import java.time.Instant
 import java.util.*
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
+import kotlinx.coroutines.async
 
 fun main() {
-    val env = System.getenv()
-    val openToken = env["OPEN_TOKEN"]
-    val secret = env["SECRET"]
-    if (openToken == null || secret == null) {
-        println("環境変数をセットしてください")
-        return
-    }
-
-    val nonce = UUID.randomUUID().toString()
-    val time = "" + Instant.now().toEpochMilli()
-    val data = openToken + time + nonce
-    val secretKeySpec = SecretKeySpec(secret.toByteArray(), "HmacSHA256")
-    val mac = Mac.getInstance("HmacSHA256")
-    mac.init(secretKeySpec)
-
-    val signature = String(Base64.getEncoder().encode(mac.doFinal(data.toByteArray())))
-
     val client = HttpClient(OkHttp) {
         install(ContentNegotiation) {
             json()
         }
     }
 
+    val headers = generateHeaders()
     val apiHost = "https://api.switch-bot.com"
     val getDevicesList = "$apiHost/v1.1/devices"
+     println("start request")
+     runBlocking {
+         val responseDeferred = async {
+            client.request(getDevicesList) {
+                method = HttpMethod.Get
+                this.headers.appendAll(headers)
+            } 
+         }
+         val response = responseDeferred.await()
+         println(response.body<Response>())
 
-    println("start request")
-    runBlocking {
-        // 持っているデバイス一覧を取得
-        val response = client.request(getDevicesList) {
-            method = HttpMethod.Get
-            headers {
-                append(HttpHeaders.Authorization, openToken)
-                append("sign", signature)
-                append("nonce", nonce)
-                append("t", time)
-            }
-        }
-
-        println(response.body<Response>())
-
-        // deviceIdを取得する
-        val deviceId = response.body<Response>().body?.deviceList?.find { it.deviceType == "Bot" }?.deviceId
-        if (deviceId == null) {
-            println("Botが見つかりませんでした")
-            return@runBlocking
-        }
-
-        // Botをオンにする
-        val sendCommand = "$apiHost/v1.1/devices/$deviceId/commands"
-        client.request(sendCommand) {
-            method = HttpMethod.Post
-            headers {
-                append(HttpHeaders.Authorization, openToken)
-                append("sign", signature)
-                append("nonce", nonce)
-                append("t", time)
-            }
-            contentType(ContentType.Application.Json)
-            setBody(
-                BotCommand(
-                    command = "turnOn",
-                    parameter = "default",
-                    commandType = "command"
+         val deviceId = response.body<Response>().body?.deviceList?.find { it.deviceType == "Bot" }?.deviceId
+            ?: throw RuntimeException("Botが見つかりませんでした")
+         val sendCommand = "$apiHost/v1.1/devices/$deviceId/commands"
+         val sendCommandDeferred = async {
+            client.request(sendCommand) {
+                method = HttpMethod.Post
+                this.headers.appendAll(headers)
+                contentType(ContentType.Application.Json)
+                setBody(
+                    BotCommand(
+                        command = "turnOn",
+                        parameter = "default",
+                        commandType = "command"
+                    )
                 )
-            )
-        }
-        client.close()
+            }
+         }
+         sendCommandDeferred.await()
+         client.close()
+         println("end request")
+    }
+}
+
+private fun generateHeaders(): Headers {
+    // 環境変数を取得
+    val env = System.getenv()
+    val openToken = env["OPEN_TOKEN"]
+    val secret = env["SECRET"]
+    // 環境変数がセットされていない場合は例外をスロー
+    if (openToken == null || secret == null) {
+        throw RuntimeException("環境変数をセットしてください")
+    }
+    // ランダムなnonceを生成
+    val nonce = UUID.randomUUID().toString()
+    // 現在の時間をミリ秒で取得
+    val time = "" + Instant.now().toEpochMilli()
+    // データを作成
+    val data = openToken + time + nonce
+    // シークレットキーを作成
+    val secretKeySpec = SecretKeySpec(secret.toByteArray(), "HmacSHA256")
+    // Macインスタンスを作成し、シークレットキーで初期化
+    val mac = Mac.getInstance("HmacSHA256")
+    mac.init(secretKeySpec)
+    // データをHMAC-SHA256で署名
+    val signature = String(Base64.getEncoder().encode(mac.doFinal(data.toByteArray())))
+    // ヘッダーを作成して返す
+    return headers {
+        append(HttpHeaders.Authorization, openToken)
+        append("sign", signature)
+        append("nonce", nonce)
+        append("t", time)
     }
 }
 
 @Serializable
 data class Response(
-    val statusCode: String,
+    val statusCode: String? = null,
     val body: DeviceList? = null,
     val message: String
 )
